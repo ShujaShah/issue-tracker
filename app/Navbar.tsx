590a391d86eaf2e105a7f458cb786781dd1f2cea
ShujaShah <shuja.ashraf11@gmail.com>
2024-08-07 23:51:52 +0530

    Active link in the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,14 +1,18 @@
+'use client';
+
 import Link from 'next/link';
-import React from 'react';
+import { usePathname } from 'next/navigation';
 import { SiHelpscout } from 'react-icons/si';
+import classnames from 'classnames';
 
 const Navbar = () => {
+  const currentPath = usePathname();
   const links = [
     { label: 'Dashboard', href: '/' },
     { label: 'Issues', href: '/issues' },
   ];
   return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+    <nav className="xlex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
         <SiHelpscout />
       </Link>
@@ -17,7 +21,9 @@ const Navbar = () => {
           <Link
             key={link.href}
             href={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={`${
+              link.href === currentPath ? 'text-zinc-900' : 'text-zinc-500'
+            } hover:text-zinc-800 transition-colors`}
           >
             {link.label}
           </Link>
