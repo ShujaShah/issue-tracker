e7637fdcfa5d7a3ce2c2bf32841c31a6bdd5860e
ShujaShah <shuja.ashraf11@gmail.com>
2024-08-14 00:08:12 +0530

    implemented loader to all the pages

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import { Box, Card, Flex, Skeleton } from '@radix-ui/themes';
 
 const LoadingIssueDetailsPage = () => {
-  return <div>loading</div>;
+  return (
+    <Box>
+      <Skeleton className="max-w-xl" />
+      <Flex className="space-x-3" my="2">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className="prose mt-4">
+        <Skeleton>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          felis tellus, efficitur id convallis a, viverra eget libero. Nam magna
+          erat, fringilla sed commodo sed, aliquet nec magna.
+        </Skeleton>
+      </Card>
+    </Box>
+  );
 };
 
 export default LoadingIssueDetailsPage;
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,27 @@
+import { Container, Text, Flex, Skeleton } from '@radix-ui/themes';
 import React from 'react';
 
 const LoadingNewIssuesPage = () => {
-  return <div>Loading...</div>;
+  return (
+    <div>
+      <Container size="1">
+        <Flex direction="column" gap="2">
+          <Text>
+            <Skeleton>Lorem ipsum dolor sit amet.</Skeleton>
+          </Text>
+
+          <Skeleton>
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque felis tellus, efficitur id convallis a, viverra eget
+              libero. Nam magna erat, fringilla sed commodo sed, aliquet nec
+              magna.
+            </Text>
+          </Skeleton>
+        </Flex>
+      </Container>
+    </div>
+  );
 };
 
 export default LoadingNewIssuesPage;
